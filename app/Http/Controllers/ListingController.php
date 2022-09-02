d93b3606d2cfb069809b0078530c8bcb351c3fde
Bunyaminugur <bunyamin26@outlook.com>
2022-09-02 17:36:10 +0200

    Move layout to component

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+use Illuminate\View\FileViewFinder;
+
+class ListingController extends Controller
+{
+    // show all listings
+    public function index()
+    {
+        return view('listings.index', [
+            'listings' => Listing::all(),
+        ]);
+    }
+
+    // show single listing
+    public function show(Listing $listing)
+    {
+        return view('Listings.show', [
+            'listing' => $listing,
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +16,16 @@ use App\Models\Listing;
 |
 */
 // All listings
-Route::get('/', function () {
-    return view('listings', [
-        'listings' => Listing::all(),
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single listing
-Route::get('/listings/{listing}' , function (Listing $listing) {
-        return view('Listing' , [
-            'listing' => $listing,
-        ]);
-});
+Route::get('/listings/{listing}' , [ListingController::class, 'show']);
+
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing
