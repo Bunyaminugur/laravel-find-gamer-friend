5d763c64aac24b5753d9094bfb94532e8884477d
Bunyaminugur <bunyamin26@outlook.com>
2022-09-04 04:01:32 +0200

    Add file upload

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -7,7 +7,7 @@
             <p class="mb-4">Find your gamer buddy</p>
         </header>
 
-        <form method="POST" action="/listings">
+        <form method="POST" action="/listings" enctype="multipart/form-data">
             @csrf
             <div class="mb-6">
                 <label for="company" class="inline-block text-lg mb-2">Gamer Title</label>
@@ -57,10 +57,13 @@
                 @enderror
             </div>
 
-            {{-- <div class="mb-6">
+            <div class="mb-6">
                 <label for="logo" class="inline-block text-lg mb-2">Gamer Logo</label>
                 <input type="file" class="border border-gray-200 rounded p-2 w-full" name="logo"/>
-            </div> --}}
+                @error('logo')
+                <p class='text-red-500 text-xs mt-1'>{{$message}}</p>
+                @enderror
+            </div>
 
             <div class="mb-6">
                 <label for="description" class="inline-block text-lg mb-2">Gamer Description</label>
